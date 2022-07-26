f6af549105a05faa2e836144cde922f5a344e4f0
Felipe Antero <souzantero@gmail.com>
2022-07-26 16:50:18 -0300

    feat(prisma): add restocking models

--- server/prisma/migrations/20220725193510_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateTable
-CREATE TABLE "Food" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL
-);
---
+++ server/prisma/migrations/20220726194837_init/migration.sql
@@ -0,0 +1,32 @@
+-- CreateTable
+CREATE TABLE "Food" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL,
+    "updatedAt" DATETIME NOT NULL,
+    "deletedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "FoodRestocking" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL,
+    "updatedAt" DATETIME NOT NULL,
+    "deletedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "RestockedFood" (
+    "foodRestockingId" TEXT NOT NULL,
+    "foodId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL,
+    "updatedAt" DATETIME NOT NULL,
+    "deletedAt" DATETIME NOT NULL,
+
+    PRIMARY KEY ("foodRestockingId", "foodId"),
+    CONSTRAINT "RestockedFood_foodRestockingId_fkey" FOREIGN KEY ("foodRestockingId") REFERENCES "FoodRestocking" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "RestockedFood_foodId_fkey" FOREIGN KEY ("foodId") REFERENCES "Food" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Food_name_key" ON "Food"("name");
