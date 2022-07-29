3cdd255142441383927d6dfb7de085459f71b367
Felipe Antero <souzantero@gmail.com>
2022-07-29 12:01:00 -0300

    feat(client/food): add food button created

--- client/src/app/components/Foods.tsx
+++ client/src/app/components/Foods.tsx
@@ -1,4 +1,4 @@
-import { Skeleton } from '@chakra-ui/react'
+import { Box, Button, ButtonGroup, Container, Flex, Heading, Skeleton, Spacer } from '@chakra-ui/react'
 
 import { useMemo } from "react"
 import { useFoods } from "../hooks/useFoods"
@@ -11,6 +11,19 @@ export function Foods() {
   if (fetching) return <Skeleton height='20px' />
 
   return (
-    <FoodTable foods={foods || []} />
+    <Container padding='2'>
+      <Flex padding='2' minWidth='max-content' alignItems='center' gap='2'>
+        <Box padding='2'>
+          <Heading size='md'>Alimentos</Heading>
+        </Box>
+        <Spacer />
+        <ButtonGroup gap='2'>
+          <Button colorScheme='red'>Adicionar</Button>
+        </ButtonGroup>
+      </Flex>
+      <Box marginTop='2'>
+        <FoodTable foods={foods || []} />
+      </Box>
+    </Container>
   )
 }
