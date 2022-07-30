f3e4380e5fd6ecc31b82e6e1c45a428e4e856329
Felipe Antero <souzantero@gmail.com>
2022-07-30 13:47:07 -0300

    feat(client/food): handle load foods error

--- client/src/app/components/food/FoodScaffold.tsx
+++ client/src/app/components/food/FoodScaffold.tsx
@@ -1,14 +1,28 @@
-import { Box, Button, ButtonGroup, Container, Flex, Heading, Skeleton, Spacer, useBoolean } from '@chakra-ui/react'
+import { Box, Button, ButtonGroup, Container, Flex, Heading, Skeleton, Spacer, useBoolean, useToast } from '@chakra-ui/react'
+import { useEffect } from 'react'
 import { useFoods } from "../../hooks/useFoods"
 import { AddFoodModal } from './AddFoodModal'
 import { FoodTable } from './FoodTable'
 
 export function FoodScaffold() {
+  const toast = useToast()
   const [isAdd, isAddAction] = useBoolean()
-  const { foods, isLoading } = useFoods()
+  const { foods, isLoading, error } = useFoods()
   const handleClickAdd = () => {
     isAdd ? isAddAction.off() : isAddAction.on()
   }
+
+  useEffect(() => {
+    if (error) {
+      const description = error.message ? error.message : ''
+      
+      toast({
+        status: 'error',
+        title: 'Falha ao buscar alimentos.',
+        description
+      })
+    }
+  }, [error])
 
   if (isLoading) return <Skeleton height='20px' />
   return (
