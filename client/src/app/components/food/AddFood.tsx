ae478ee3114deea2d0fe670475f891a3cc8b9248
Felipe Antero <souzantero@gmail.com>
2022-08-10 20:17:01 -0300

    feat(client/food): make AddFood working with FoodForm

--- client/src/app/components/food/AddFood.tsx
+++ client/src/app/components/food/AddFood.tsx
@@ -1,39 +1,21 @@
-import { ChangeEvent, FormEvent, useState } from "react"
-import { Flex, FormControl, FormLabel, Input, Select, Spacer, Stack } from '@chakra-ui/react'
+import { useState } from "react"
 import { useAddFood } from "../../hooks/useAddFood"
 import { useNavigate } from "react-router-dom"
 import { Page } from "../layout/Page"
 import { PageHeader } from "../layout/PageHeader"
 import { PageBody } from "../layout/PageBody"
-import { ActionButton } from "../button/ActionButton"
-import { parseIntOrZeroIfNaN } from "../../../domain/utils"
-
-enum Period {
-  Day = 1,
-  Month = 30,
-  Year = 365
-}
+import { FoodForm, Period } from "./FoodForm"
 
 export function AddFood() {
   const navigate = useNavigate()
   const [name, setName] = useState<string>('')
-  const [expiresIn, setExpiresIn] = useState<string>('')
+  const [expiresIn, setExpiresIn] = useState<number>(0)
   const [period, setPeriod] = useState<Period>(Period.Day)
   
   const { addFood, isAdding } = useAddFood()
 
-  const handleChangeName = (event: ChangeEvent<HTMLInputElement>) => setName(event.target.value)
-  const handleChangeExpiresIn = (event: ChangeEvent<HTMLInputElement>) => setExpiresIn(event.target.value)
-  const handleChangePeriod = (event: ChangeEvent<HTMLSelectElement>) => {
-    const period = Object.values(Period).find(key => key.toString() === event.target.value) || '1'
-    const periodValue = typeof period === 'string' ? parseInt(period) : period
-    setPeriod(periodValue)
-  }
-
-  const handleSubmit = async (event: FormEvent) => {
-    event.preventDefault()
-    const expiresInInt = parseIntOrZeroIfNaN(expiresIn)
-    const params = { name, expiresIn: expiresInInt * period }
+  const handleSubmit = async () => {    
+    const params = { name, expiresIn: expiresIn * period }
     const createdFood = await addFood(params)
     if (createdFood) {
       clear()
@@ -43,7 +25,7 @@ export function AddFood() {
 
   const clear = () => {
     setName('')
-    setExpiresIn('')
+    setExpiresIn(0)
     setPeriod(Period.Day)
   }
 
@@ -51,40 +33,21 @@ export function AddFood() {
     <Page>
       <PageHeader title="Adicionar alimento"/>
       <PageBody>
-        <Stack as={'form'} spacing={4} onSubmit={handleSubmit}>
-          <FormControl isRequired isDisabled={isAdding}>
-            <FormLabel>Nome</FormLabel>
-            <Input type='text' value={name} onChange={handleChangeName} />
-          </FormControl>
-
-          <Stack direction={'row'}>
-            <FormControl isRequired isDisabled={isAdding}>
-              <FormLabel>Validade</FormLabel>
-              <Input type='number' value={expiresIn}  min={1} onChange={handleChangeExpiresIn}/>
-            </FormControl>
-
-            <FormControl isDisabled={isAdding}>
-              <FormLabel>Período</FormLabel>
-              <Select value={period} onChange={handleChangePeriod}>
-                <option value='1'>Dia(s)</option>
-                <option value='30'>Mês(es)</option>
-                <option value='365'>Ano(s)</option>
-              </Select>
-            </FormControl>
-          </Stack>
-          
-          <Flex minWidth={'max-content'} alignItems={'end'} gap={2}>
-            <Spacer/>
-            <ActionButton
-              type='submit'
-              isDisabled={isAdding}
-              isLoading={isAdding}
-              loadingText='Enviando...'
-            >
-              Enviar
-            </ActionButton>
-          </Flex>
-        </Stack>
+        <FoodForm
+          value={{
+            name,
+            expiresIn,
+            period
+          }}
+          onChange={(data) => {
+            setName(data.name)
+            setExpiresIn(data.expiresIn)
+            setPeriod(data.period)
+          }}
+          onSubmit={handleSubmit}
+          isDisabled={isAdding}
+          isLoading={isAdding}
+        />
       </PageBody>
     </Page>
   )
---
+++ client/src/app/components/food/FoodForm.tsx
@@ -0,0 +1,100 @@
+import { ChangeEvent, FormEvent, useState } from "react"
+import { Flex, FormControl, FormLabel, Input, Select, Spacer, Stack } from '@chakra-ui/react'
+import { ActionButton } from "../button/ActionButton"
+import { parseIntOrZeroIfNaN } from "../../../domain/utils"
+
+export enum Period {
+  Day = 1,
+  Month = 30,
+  Year = 365
+}
+
+export interface FoodFormData {
+  name: string
+  expiresIn: number
+  period: Period
+}
+
+export interface FoodFormProps {
+  value: FoodFormData
+  onChange: (data: FoodFormData) => void
+  onSubmit: () => void
+  isDisabled?: boolean
+  isLoading?: boolean
+  buttonText?: string
+  buttonLoadingText?: string
+}
+
+export function FoodForm({
+  isDisabled,
+  isLoading,
+  value,
+  onChange,
+  onSubmit,
+  buttonText,
+  buttonLoadingText
+}: FoodFormProps) {
+  
+  const handleChangeName = (event: ChangeEvent<HTMLInputElement>) => {
+    const newValue = {...value}
+    newValue.name = event.target.value
+    onChange(newValue)
+  }
+
+  const handleChangeExpiresIn = (event: ChangeEvent<HTMLInputElement>) => {
+    const expiresInInt = parseIntOrZeroIfNaN(event.target.value)
+    const newValue = {...value}
+    newValue.expiresIn = expiresInInt
+    onChange(newValue)
+  }
+
+  const handleChangePeriod = (event: ChangeEvent<HTMLSelectElement>) => {
+    const period = Object.values(Period).find(key => key.toString() === event.target.value) || '1'
+    const periodValue = typeof period === 'string' ? parseInt(period) : period
+    const newValue = {...value}
+    newValue.period = periodValue
+    onChange(newValue)
+  }
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault()
+    onSubmit()
+  }
+
+  return (
+    <Stack as={'form'} spacing={4} onSubmit={handleSubmit}>
+      <FormControl isRequired isDisabled={isDisabled}>
+        <FormLabel>Nome</FormLabel>
+        <Input type='text' value={value.name} onChange={handleChangeName} />
+      </FormControl>
+
+      <Stack direction={'row'}>
+        <FormControl isRequired isDisabled={isDisabled}>
+          <FormLabel>Validade</FormLabel>
+          <Input type='number' value={value.expiresIn || ''}  min={1} onChange={handleChangeExpiresIn}/>
+        </FormControl>
+
+        <FormControl isDisabled={isDisabled}>
+          <FormLabel>Período</FormLabel>
+          <Select value={value.period} onChange={handleChangePeriod}>
+            <option value='1'>Dia(s)</option>
+            <option value='30'>Mês(es)</option>
+            <option value='365'>Ano(s)</option>
+          </Select>
+        </FormControl>
+      </Stack>
+          
+      <Flex minWidth={'max-content'} alignItems={'end'} gap={2}>
+        <Spacer/>
+        <ActionButton
+          type='submit'
+          isDisabled={isDisabled}
+          isLoading={isLoading}
+          loadingText={buttonLoadingText || 'Enviando...'}
+        >
+          {buttonText || 'Enviar'}
+        </ActionButton>
+      </Flex>
+    </Stack>
+  )
+}
