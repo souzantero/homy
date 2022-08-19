72057f20d6ac840e19f7763797270e0b72831de7
Felipe Antero <souzantero@gmail.com>
2022-08-19 10:50:51 -0300

    refactor(client): signed user local storage repository

--- client/src/infra/repositories/local-storage/signed-user-local-storage-repository.ts
+++ client/src/infra/repositories/local-storage/signed-user-local-storage-repository.ts
@@ -2,7 +2,7 @@ import { User } from "../../../domain/models/user";
 import { UpdateSignedUserRepository } from "../../../domain/repositories/update-signed-user-repository";
 
 export class SignedUserLocalStorageRepository implements UpdateSignedUserRepository {
-  private readonly key = 'SignedUser'
+  private readonly key = 'signed-user'
 
   async updateSignedUser(user: User): Promise<void> {
     localStorage.setItem(this.key, JSON.stringify(user))
