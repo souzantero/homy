68e133fca56c207e1748f350077d07d8ccd888a8
Felipe Antero <souzantero@gmail.com>
2022-09-09 09:17:24 -0300

    feat(client/cypress): test authentication user flow

--- client/cypress/e2e/app.spec.cy.ts
+++ client/cypress/e2e/app.spec.cy.ts
@@ -10,38 +10,45 @@ describe('app', () => {
   })
 
   describe('authentication', () => {
-    describe('sign-up', () => {
-      it('should sign up a new user', () => {
-        cy.visit(host)
-        cy.get('a').contains('Entrar').should('not.be.disabled').click()
-        cy.url().should('include', '/auth/sign-in')
-        cy.get('a').contains('Cadastre-se').should('not.be.disabled').click()
-        cy.url().should('include', '/auth/sign-up')
-
-        const name = Date.now().toString()
-        cy.get('#name').type(name)
-        cy.get('#email').type(`${name}@gmail.com`)
-        cy.get('#password').type('12345678')
-        cy.get('#confirmed-password').type('12345678')
-        cy.get('button').contains('Cadastrar').click()
-
-        cy.url().should('be.eq', `${host}/`)
-        cy.contains(name)
-      })
-    })
-  })
-
-  describe('foods', () => {
-    it('should can see a list of foods', () => {
+    it('should sign up, sign out, sign in and sign out again with new user', () => {
       cy.visit(host)
-      cy.contains('Alimentos').click()
-      cy.url().should('include', '/foods')
-      cy.contains('Banana')
-      cy.contains('5')
-      cy.contains('Maçã')
-      cy.contains('14')
-      cy.contains('Mamão')
-      cy.contains('7')
+      cy.get('a').contains('Entrar').should('not.be.disabled').click()
+      cy.url().should('include', '/auth/sign-in')
+      cy.get('a').contains('Cadastre-se').should('not.be.disabled').click()
+      cy.url().should('include', '/auth/sign-up')
+
+      const name = Date.now().toString()
+      cy.get('#name').type(name)
+      cy.get('#email').type(`${name}@gmail.com`)
+      cy.get('#password').type('12345678')
+      cy.get('#confirmed-password').type('12345678')
+      cy.get('button').contains('Cadastrar').click()
+
+      cy.url().should('be.eq', `${host}/`)
+      cy.get('.user-menu-list').should('not.be.visible')
+      cy.get('.user-menu-button').contains(name)
+      cy.get('.user-menu-button').contains('Usuário')
+      cy.get('.user-menu-button').click()
+      cy.get('.user-menu-list').should('be.visible').get('button').contains('Sair').click()
+      cy.get('.sign-out-alert-dialog').should('be.visible')
+      cy.get('.sign-out-alert-dialog').contains('Sair da conta')
+      cy.get('.sign-out-alert-dialog').contains('Você tem certeza que deseja sair da sua conta?')
+      cy.get('.sign-out-alert-dialog').get('button').contains('Cancelar').should('not.be.disabled')
+      cy.get('.confirm-user-sign-out-button').contains('Sair').should('not.be.disabled').should('be.visible').click()
+
+      cy.url().should('be.eq', `${host}/`)
+      cy.get('a').contains('Entrar').should('not.be.disabled').click()
+      cy.url().should('include', '/auth/sign-in')
+      cy.get('#email').type(`${name}@gmail.com`)
+      cy.get('#password').type('12345678')
+      cy.get('button').contains('Entrar').click()
+
+      cy.url().should('be.eq', `${host}/`)
+      cy.get('.user-menu-button').click()
+      cy.get('.user-menu-list').should('be.visible').get('button').contains('Sair').click()
+      cy.get('.confirm-user-sign-out-button').contains('Sair').should('not.be.disabled').should('be.visible').click()
+      cy.url().should('be.eq', `${host}/`)
+      cy.get('a').contains('Entrar').should('not.be.disabled').should('be.visible')
     })
   })
 })
