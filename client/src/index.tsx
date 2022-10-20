1453b1082efa2c911c6f764836165dc7e7c44588
Felipe Antero <souzantero@gmail.com>
2022-10-20 17:39:42 -0300

    chore(client): move contexts to index

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
@@ -7,7 +10,13 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <QueryClientProvider client={new QueryClient()}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ChakraProvider>
   </React.StrictMode>
 )
 
