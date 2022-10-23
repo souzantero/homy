b2abce6c87af0a5fd701b5b6dc38ea03a115533a
Felipe Antero <souzantero@gmail.com>
2022-10-23 10:45:35 -0300

    chore(server): add and configure serverless framework

---
+++ server/src/lambda.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core'
+import { PrismaClient } from '@prisma/client'
+import * as serverless from 'serverless-http'
+import { AppModule } from './app/app.module'
+
+let server
+const prisma: PrismaClient = new PrismaClient()
+
+export async function handler(event: any, context) {
+  if (!server) {
+    const app = await NestFactory.create(AppModule.register({ prisma }))
+    await app.init()
+    server = serverless(app.getHttpAdapter().getInstance())
+  }
+
+  return server(event, context)
+}
